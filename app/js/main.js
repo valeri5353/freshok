8b83b382921fe578cd3fc4284436e14fa24a7a81
valeri5353 <valeri5353@gmail.com>
2022-02-14 12:43:07 +0200

    added homework12

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$ (function(){
+$(function () {
+    $('.slider__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        fade: true,
+        infinite: false,
+        // autoplay: true,
+        // autoplaySpeed: 4000
+    });
 
+    $('.product').hover( function () {
+        $(this).toggleClass('product--active');
+        $(this).find('.product__img').toggleClass('product__img--active');  
+    });
+
+    let Mixer = mixitup('.top-products__inner');
 });
